f3ad080435f92784d880b942aad26cd3e3ace364
deanrobin333 <deanrobin777@gmail.com>
2023-08-11 02:30:35 +0300

    task 6 - sonsole.py

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Entry point of the command interpreter"""
+import cmd
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """ General Class for HBNBCommand """
+    prompt = '(hbnb) '
+    dic_class = {'BaseModel': BaseModel}
+
+    def do_quit(self, arg):
+        """ Exit method for quit typing """
+        exit()
+
+    def do_EOF(self, arg):
+        """ Exit method for EOF """
+        print('')
+        exit()
+
+    def emptyline(self):
+        """ Method to pass when emptyline entered """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
