bba2988dc18b11fcfa2fef4e0cbc3e175f914cc2
deanrobin333 <deanrobin777@gmail.com>
2023-08-11 01:50:58 +0300

    models/base_model.py, models/__init__.py, tests/

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a class BaseModel"""
+import uuid
+from datetime import datetime
+import string
+
+
+class BaseModel:
+    """ Class that defines properties of base """
+    models = []
+    id = str(uuid.uuid4())
+    created_at = datetime.datetime.now()
+    updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """Returns a string represation of class details."""
+        return f'BaseModel({self.id}, {self.__dict__})'
+
+    def save_model(self):
+        """Update public instance attribute updated_at with current datetime.
+        """
+        self.updated_at = datetime.datetime.now()
+        self.created_at = self.created_at
+        BaseModel.models.append(self)
+
+    def to_dict(self):
+        """Returns a dictionary containing all key/values of __dict__ of
+        the instance."""
+        model_dict = {
+            "id": self.id,
+            "time created": self.created_at.isoformat(),
+            "time updated": self.updated_at.isoformat()
+        }
+
+        return model_dict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" Defines a class TestBaseModel for BaseModel module. """
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """Defines tests for Amenity Class"""
+
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.BaseModel1 = BaseModel()
+        cls.BaseModel1.name = "Samsung"
+        cls.BaseModel1.my_number = 89
+
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.BaseModel1
+
+    def test_class_exists(self):
+        """Tests if class exists.
+        """
+        result = "<class 'models.base_model.BaseModel'>"
+        self.assertEqual(str(type(self.BaseModel1)), result)
+
+    def test_types(self):
+        """Test if attributes type is correct.
+        """
+        self.assertIsInstance(self.BaseModel1.name, str)
+        self.assertEqual(type(self.BaseModel1.name), str)
+        self.assertIsInstance(self.BaseModel1.id, str)
+        self.assertEqual(type(self.BaseModel1.id), str)
+        self.assertIsInstance(self.BaseModel1.created_at, datetime.datetime)
+        self.assertIsInstance(self.BaseModel1.updated_at, datetime.datetime)
+
+    def test_save(self):
+        """Test if save method is working correctly after update.
+        """
+        self.BaseModel1.save()
+        self.assertNotEqual(self.BaseModel1.created_at,
+                            self.BaseModel1.updated_at)
+
+    def test_functions(self):
+        """Test if BaseModel moudule is documented.
+        """
+        self.assertIsNotNone(BaseModel.__doc__)
+
+    def test_has_attributes(self):
+        """Test if expected attributes exist.
+        """
+        self.assertTrue(hasattr(self.BaseModel1, 'name'))
+        self.assertTrue(hasattr(self.BaseModel1, 'id'))
+        self.assertTrue(hasattr(self.BaseModel1, 'created_at'))
+        self.assertTrue(hasattr(self.BaseModel1, 'updated_at'))
+
+    def test_set_attributes(self):
+        """Test set attributes of BaseModel.
+        """
+        self.assertEqual(self.BaseModel1.name, "Samsung")
+        self.assertEqual(self.BaseModel1.my_number, 89)
+
+
+if __name__ == '__main__':
+    unittest.main()
