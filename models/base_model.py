82ead947f2612f033efd073f02bcbca759979c3b
deanrobin333 <deanrobin777@gmail.com>
2023-08-11 02:09:36 +0300

    task 4 - models/base_model.py - update

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,54 @@
 #!/usr/bin/python3
-"""Defines a class BaseModel"""
+"""Defines a class Base"""
 import uuid
 from datetime import datetime
-import string
+import models
 
 
 class BaseModel:
     """ Class that defines properties of base """
-    models = []
-    id = str(uuid.uuid4())
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        """ Creates new instances of Base """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+        else:
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for (key, value) in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
-        """Returns a string represation of class details."""
-        return f'BaseModel({self.id}, {self.__dict__})'
+        """Returns a string represation of class details.
+
+        Returns:
+            str: class details
+        """
+        string = "["
+        string += str(self.__class__.__name__) + '] ('
+        string += str(self.id) + ') ' + str(self.__dict__)
+        return string
 
     def save(self):
         """Update public instance attribute updated_at with current datetime.
         """
-        self.updated_at = datetime.datetime.now()
-        self.created_at = self.created_at
-        BaseModel.models.append(self)
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all key/values of __dict__ of
-        the instance."""
-        model_dict = {
-            "id": self.id,
-            "time created": self.created_at.isoformat(),
-            "time updated": self.updated_at.isoformat()
-        }
+        the instance.
 
-        return model_dict
+        Returns:
+            dict: key/value pairs.
+        """
+        dict_ = self.__dict__.copy()
+        dict_['__class__'] = self.__class__.__name__
+        dict_['created_at'] = self.created_at.isoformat()
+        dict_['updated_at'] = self.updated_at.isoformat()
+        return dict_
