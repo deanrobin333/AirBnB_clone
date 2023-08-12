823e4bac126329bcb7eec5e5f6776465f8f510a1
Peacecole <onyangocollince@gmail.com>
2023-08-12 13:42:26 +0300

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+"""Defines a class Amenity that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class that defines properties of Amenity.
+
+    Attributes:
+        name (string): name of amenity.
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Creates new instances of Amenity.
+        """
+        super().__init__(*args, **kwargs)
