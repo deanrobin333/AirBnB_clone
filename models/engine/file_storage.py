efbb5a1378aeb2be9a60403f4a4169681b195179
deanrobin333 <deanrobin777@gmail.com>
2023-08-13 23:54:43 +0300

    added models/engine/__init__py, modified file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,15 @@
 import json
 import os
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-class FileStorage:
+class FileStorage():
     """Class that serializes instances to a JSON file and deserializes
     JSON file to instances.
     """
@@ -20,6 +26,7 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary objects.
+
         Returns:
             dict: objects.
         """
@@ -27,6 +34,7 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id.
+
         Args:
             obj (any): object.
         """
@@ -38,10 +46,10 @@ class FileStorage:
         """
         dictionary = {}
 
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             dictionary[key] = value.to_dict()
 
-        with open(self.__file_path, 'w', encoding="utf-8") as myFile:
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as myFile:
             json.dump(dictionary, myFile)
 
     def reload(self):
@@ -49,7 +57,22 @@ class FileStorage:
         (__file_path) exists ; otherwise, do nothing. If the file doesn’t
         exist, no exception should be raised)
         """
-        if os.path.exists(self.__file_path) is True:
-            with open(self.__file_path, 'r', encoding="utf-8") as myFile:
-                for key, value in json.load(myFile).items():
-                    self.new(BaseModel(**value))
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as myFile:
+                my_obj_dump = myFile.read()
+        except Exception:
+            return
+        objects = eval(my_obj_dump)
+        for (key, value) in objects.items():
+            objects[key] = eval(key.split('.')[0] + '(**value)')
+        self.__objects = objects
+
+    def delete(self, obj):
+        """Deletes obj from __objects
+        """
+        try:
+            key = obj.__class__.__name__ + '.' + str(obj.id)
+            del self.__objects[key]
+            return True
+        except Exception:
+            return False
