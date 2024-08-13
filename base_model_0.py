1b2c5d3ba6e7334d6184b96e05fe79737a8129c4
deanrobin333 <deanrobin777@gmail.com>
2024-08-13 13:28:54 +0300

    updated test_base_model.py to pass task 3

---
+++ base_model_0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# base_model_0.py
+"""Doc
+"""
+from models.tmp_base_model import *
+from models.tmp_base_model import BaseModel
+
+
+class BaseModel(BaseModel):
+    """Doc
+    """
+
+    def save(self):
+        """Doc
+        """
+        pass
