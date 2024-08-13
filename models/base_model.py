f78495e16667a4a3e4b5de3abd465feaab76059d
deanrobin333 <deanrobin777@gmail.com>
2024-08-13 13:16:18 +0300

    practise -- updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-"""Defines a class Base"""
+# base_mode.py
+
 import uuid
 from datetime import datetime
-import models
 
 
-class BaseModel:
+class BaseModel():
     """ Class that defines properties of base """
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """ Creates new instances of Base """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
-        else:
-            time_format = "%Y-%m-%dT%H:%M:%S.%f"
-            for (key, value) in kwargs.items():
-                if key in ('created_at', 'updated_at'):
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
+        if kwargs:
+            for key, value in kwargs.items():
+                self.key = value
+    
     def __str__(self):
         """Returns a string represation of class details.
 
         Returns:
             str: class details
         """
-        string = "["
-        string += str(self.__class__.__name__) + '] ('
-        string += str(self.id) + ') ' + str(self.__dict__)
-        return string
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Update public instance attribute updated_at with current datetime.
         """
         self.updated_at = datetime.now()
-        models.storage.save()
-
+    
     def to_dict(self):
         """Returns a dictionary containing all key/values of __dict__ of
         the instance.
@@ -47,8 +37,9 @@ class BaseModel:
         Returns:
             dict: key/value pairs.
         """
-        dict_ = self.__dict__.copy()
-        dict_['__class__'] = self.__class__.__name__
-        dict_['created_at'] = self.created_at.isoformat()
-        dict_['updated_at'] = self.updated_at.isoformat()
-        return dict_
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+
+        return new_dict
