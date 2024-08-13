81f32e84e8225f397038f2e641fbe9f0f8de44d9
deanrobin333 <deanrobin777@gmail.com>
2024-08-13 13:41:59 +0300

    updated base_model.py - changed initialized of updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel():
         """ Creates new instances of Base """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
         if kwargs:
             for key, value in kwargs.items():
