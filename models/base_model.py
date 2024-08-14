13cc368d32791ba63a2219fc6739bf2690053a6a
deanrobin333 <deanrobin777@gmail.com>
2024-08-14 08:05:04 +0300

    datetime utcnow for self.updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -47,7 +47,7 @@ class BaseModel():
     def save(self):
         """Update public instance attribute updated_at with current datetime.
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
