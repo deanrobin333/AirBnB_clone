966aae111c3d2cdcc5fc51293ce39d7c42f77ba0
deanrobin333 <deanrobin777@gmail.com>
2024-08-14 06:01:19 +0300

    changed time attribute

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel():
             # datetime.utcnow() has been depracated
             # datetime.now(timezone.utc) - better timezone
             #  - from datetime import timezone
-            self.created_at = datetime.now()
+            self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
             '''Add the new when created object'''
             models.storage.new(self)
@@ -47,7 +47,7 @@ class BaseModel():
     def save(self):
         """Update public instance attribute updated_at with current datetime.
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
